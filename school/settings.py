bff37a73bd764f21a25f027fbc28e1e2e42c5123
parsa <parsahashemian15603@gmail.com>
2024-08-30 23:41:35 +0330

    ijad avalin template student list va check kardan dorosty ravash anjam shode

--- school/settings.py
+++ school/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'school.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'school' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
